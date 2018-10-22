238c3985ba0d5416f3206b8b3fa975d2752afd92
Marius Seack <mariusseack@Mariuss-Air.fritz.box>
2018-10-22 17:16:56 +0200

    Setup startscript and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
@@ -59,7 +61,7 @@ app.get("/bad", (req, res) => {
     errorMessage: "This site does not exist"
   });
 });
-//listen to port 3000
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+//listen to port 3000 (localhost)
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
